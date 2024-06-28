470deb1f6e1078ef8ace227e1f52db68328d8373
yohei-ozao <yohei.ozao@woven.toyota>
2024-06-28 15:02:03 +0900

    Add 04_callenge.py

--- 02_file_handling_and_dataframes/04_challenge/04_challenge.py
+++ 02_file_handling_and_dataframes/04_challenge/04_challenge.py
@@ -1,1 +1,12 @@
 # ここにコードを書いてください
+import pandas as pd
+from sklearn.datasets import load_wine
+
+wine_data = load_wine()
+wine_df = pd.DataFrame(data=wine_data.data, columns=wine_data.feature_names)
+
+print(wine_df.set_index("alcohol").sort_index().head())
+
+print(wine_df[(wine_df["alcohol"]>13) & (wine_df["malic_acid"]>2)])
+
+#print(wine_df.head())
